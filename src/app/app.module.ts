78a5b0f1b303ef2ef5348c064e7f1aed64967c52
rar50 <70046877+rar50@users.noreply.github.com>
2022-09-17 18:41:13 +0900

    2 files modified, 9 added, 4 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { PageTopComponent } from './page-top/page-top.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
+  ],
+  declarations: [
+    AppComponent,
+    PageTopComponent,
+    ProductAlertsComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
